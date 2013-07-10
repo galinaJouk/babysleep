e60156b7e3d20b201fb98acd01959615a397554b
galinaJouk <galdem22@gmail.com>
2013-07-10 08:37:41 -0400

    commit new file

--- php/graph.php
+++ php/graph.php
@@ -4,20 +4,24 @@
 header ("Content-type: image/png");
 
 //create new 300x300 image
-$myImage = ImageCreate(300,300);
+$myImage = ImageCreate(300,200);
 
 //the first color allocated is the background color
-$black = ImageColorAllocate($myImage, 0, 0, 0);
 $white = ImageColorAllocate($myImage, 255, 255, 255);
 $red = ImageColorAllocate($myImage, 255, 0, 0);
-$blue = ImageColorAllocate($myImage, 0, 0, 255);
 $green = ImageColorAllocate($myImage, 0, 255, 0);
 
-//draw and fill pie
-//ImageRectangle($myImage, 100, 100, 140, 130, $white);
-ImageFilledArc($myImage, 110, 110, 200, 150, 0, 90, $red, IMG_ARC_PIE);
-ImageFilledArc($myImage, 100, 100, 200, 150, 90, 180, $green, IMG_ARC_PIE);
-ImageFilledArc($myImage, 100, 100, 200, 150, 180, 360, $blue, IMG_ARC_PIE);
+
+//draw a green rectangal around the edge of the screen
+ImageRectangle($myImage, 0, 0, 299, 199, $green);
+
+//start drawing line of graph
+Imageline($myImage, 10, 40, 50, 10, $green);
+Imageline($myImage, 50, 10, 90, 60, $green);
+Imageline($myImage, 90, 60, 130, 30, $green);
+Imageline($myImage, 130, 30, 170, 70, $green);
+Imageline($myImage, 170, 70, 210, 40, $green);
+Imageline($myImage, 210, 40, 250, 50, $green);
 
 //send the bytes of the image
 ImagePng($myImage);
